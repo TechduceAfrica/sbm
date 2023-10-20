ee6e92141f037a03f040bf2346fd8f3636c704e1
ugwuintech <106489794+ugwuintech@users.noreply.github.com>
2023-10-20 08:31:18 -0700

    UPDATED GOOGLE TAG CODE

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import Navbar from "./components/Navbar";
+import Script from "next/script";
 
 import NewFooter from "./components/NewFooter";
 import GoogleAnalytics from "./components/GoogleAnalytics";
+
+const GTM_ID = "GTM-WD23JB47";
 
 export const metadata: Metadata = {
   title: "Social Brand Managers",
@@ -17,11 +20,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <body className="bg-secondary-blue">
-        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
-          <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
-        ) : null}
-
         <Navbar />
         <main>{children}</main>
         <NewFooter />
