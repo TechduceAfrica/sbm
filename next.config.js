d40379bd8502af4df2870a163f0302e49e5de87b
ugwuintech <106489794+ugwuintech@users.noreply.github.com>
2023-10-20 05:22:10 -0700

    sitemap update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["socialbrandmanagers.com", "res.cloudinary.com", "assets-social-brand-managers.netlify.app"],
+    domains: [
+      "socialbrandmanagers.com",
+      "res.cloudinary.com",
+      "assets-social-brand-managers.netlify.app",
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemap",
+      },
+    ];
   },
 };
 
