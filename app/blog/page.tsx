f0069a80ab492ee3eed0eb1da4b684128301fcaa
ugwuintech <106489794+ugwuintech@users.noreply.github.com>
2023-10-23 06:53:47 -0700

    temporary blog page done

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../utils/motion";
 import SectionWrapper from "../hoc/SectionWrapper";
+import Button from "../components/Button";
 
 const Blog = () => {
   return (
-    <div className="h-[50vh] w-[100vw] blog-content relative z-0 ">
+    <div className="h-[75vh] w-[100vw] blog-content relative z-0 custom-container-width ">
       {/* <div className="lines">
         <div className="line"></div>
         <div className="line"></div>
@@ -17,11 +18,19 @@ const Blog = () => {
       </div> */}
       <div className="w-full h-full  z-0 ">
         <motion.h1
-          className="text-center text-primary-blue"
+          className="text-center text-primary-blue mb-[10px] text-[2em] lg:text-[3em]"
           variants={fadeIn("right", "spring", 0.5, 0.75)}
         >
           Coming Soon!
         </motion.h1>
+
+        <motion.h3
+          className="text-center text-body-text-colour mb-[60px] max-w-[810px] text-[24px] lg:text-[32px] m-3"
+          variants={fadeIn("up", "spring", 1, 0.75)}
+        >
+          We are working to bring you the best tech news and opportunities
+          accross Africa and the Globe.
+        </motion.h3>
       </div>
     </div>
   );
