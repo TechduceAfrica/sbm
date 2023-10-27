9207aea9563f87ebe94be7b56292dddbf6d326c1
ugwuintech <106489794+ugwuintech@users.noreply.github.com>
2023-10-27 02:14:51 -0700

    bug fixed

--- app/components/HomeServices.tsx
+++ app/components/HomeServices.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import { fadeIn } from "@/app/utils/motion";
 import Image from "next/image";
 import { homeServicesTop } from "../constants";
-import { homeServicesBottom } from "../constants";
+
 import Link from "next/link";
 
 const HomeServices = () => {
