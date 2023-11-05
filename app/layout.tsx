bf9d68938f27c9870d6052a3462d458e9bf7b573
ugwuintech <106489794+ugwuintech@users.noreply.github.com>
2023-11-05 14:41:00 -0800

    created _document.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,6 @@ import NewFooter from "./components/NewFooter";
 import GoogleAnalytics from "./components/GoogleAnalytics";
 import Head from "next/head";
 
-const GTM_ID = "GTM-WD23JB47";
-
 export const metadata: Metadata = {
   title: "Social Brand Managers",
   description: "Level up social media contents for your business",
@@ -40,69 +38,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="theme-color" content="#0048a0" />
-        {/* <!-- FACEBOOK META TAGS --> */}
-        <meta property="og:title" content="Social Brand Managers" />
-        <meta
-          property="og:description"
-          content="Elevate Your Brand's Social Presence with Our Expertise - Your Ultimate Social Media Companion for Enhanced Business Content."
-        />
-        <meta
-          property="og:site_name"
-          content="Social Brand Managers by Techduce Africa"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://socialbrandmanagers.com" />
-        <meta
-          property="og:image"
-          content="https://assets-social-brand-managers.netlify.app/og%20image/og-image.webp"
-        />
-        <meta property="og:image:width" content="636" />
-        <meta property="og:image:height" content="574" />
-        <meta property="og:image:type" content="image/webp" />
-        <meta property="og:locale" content="en_NG" />
-        {/* <!-- TWITTER META TAGS --> */}
-        <meta name="author" content="Techduce Africa" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@TechduceA" />
-        <meta name="twitter:creator" content="@TechduceA" />
-        <meta property="og:url" content="https://socialbrandmanagers.com" />
-        <meta
-          property="og:title"
-          content="DuceCRM | Your Key to Better Customer Relationships"
-        />
+    <div>
+      <Navbar />
 
-        <meta
-          property="og:image"
-          content="https://assets-social-brand-managers.netlify.app/og%20image/og-image.webp"
-        />
-        {/* <!-- GOOGLE INDEX TAG --> */}
-        <meta
-          name="robots"
-          content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
-        />
-        <link rel="canonical" href="https://socialbrandmanagers.com" />
-      </Head>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-      </Script>
-      <body className="bg-secondary-blue">
-        <Navbar />
+      {children}
 
-        <main className="">{children}</main>
-
-        <NewFooter />
-      </body>
-    </html>
+      <NewFooter />
+    </div>
   );
 }
